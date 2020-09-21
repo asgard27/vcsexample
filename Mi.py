c5a3b844e57f0dbe9c58881d4784a4257d99885d
asgard27 <34618892+asgard27@users.noreply.github.com>
2020-09-22 07:02:12 +0800

    Update Mi.py
    Update codes from https://github.com/line/line-bot-sdk-python/issues/37

--- Mi.py
+++ Mi.py
@@ -7,6 +7,35 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+# below was copied
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+# above was copied
+
 
 if __name__ == '__main__':
     app.run()
